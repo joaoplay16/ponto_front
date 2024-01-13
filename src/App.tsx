7eb597370379b68e6ebf501e6f4788eaa3c7aa38
João Pedro <joaoplay16@gmail.com>
2024-01-12 23:45:39 -0300

    Cria componente Header
    Barra de navegação do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react"
+import { Header, SideBarForUser } from "./components"
+
 function App() {
+  const [isMenuOpen, setMenuOpen] = useState(true)
+
   return (
-    <div className="App">
-        <p className="bg-orange-500">
-          Em ponto app
-        </p>
+    <div className="flex h-screen ">
+      <SideBarForUser
+        className={`w-0  bg-gray-700 ${
+          isMenuOpen ? "md:w-52" : "md:w-0"
+        } flex-shrink-0 duration-200 `}
+      />
+      <div className="h-full w-full">
+        <Header
+          className="bg-gray-700 md:flex-grow"
+          isMenuOpen={isMenuOpen}
+          openMenu={() => {
+            setMenuOpen(!isMenuOpen)
+          }}
+        />
+      </div>
     </div>
   )
 }
