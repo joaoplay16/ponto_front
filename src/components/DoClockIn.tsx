f4e6f414a803e9cb1f0d307bbd916c6411fdc5af
João Pedro <joaoplay16@gmail.com>
2024-01-13 21:57:23 -0300

    Implementa registro de ponto no componente DoClockIn

--- src/components/DoClockIn.tsx
+++ src/components/DoClockIn.tsx
@@ -1,34 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, MouseEvent } from "react"
+import { apiService } from "../services/apiService"
 
 const DoClockIn = () => {
-  const [currentDateTime, setCurrentDateTime] = useState(new Date());
+  const [currentDateTime, setCurrentDateTime] = useState(new Date())
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentDateTime(new Date());
-    }, 1000);
+      setCurrentDateTime(new Date())
+    }, 1000)
 
-    return () => clearInterval(intervalId);
-  }, []);
+    return () => clearInterval(intervalId)
+  }, [])
 
-  const formattedDateTime = currentDateTime.toLocaleString('pt-BR', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-  });
+  const handleDoClokIn = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    apiService
+      .doClockIn(1)
+      .then(() => {
+        console.log("Sucesso ao bater o ponto")
+      })
+      .catch((error) => {
+        console.log("Erro ao bater o ponto")
+      })
+  }
+
+  const formattedDateTime = currentDateTime.toLocaleString("pt-BR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+  })
 
   return (
     <div className="flex h-64 w-64 flex-col items-center justify-between rounded-xl bg-gray-200 p-3 text-slate-600 shadow-md">
-      <h4 className="font-light text-center">{formattedDateTime}</h4>
-      <button className="h-32 w-32 rounded-full bg-slate-600  focus:ring-2 focus:ring-orange-300"></button>
+      <h4 className="text-center font-light">{formattedDateTime}</h4>
+      <button
+        onClick={handleDoClokIn}
+        className="h-32 w-32 rounded-full bg-slate-600  focus:ring-2 focus:ring-orange-300"></button>
       <h4 className="text-center font-light ">
         Clique aqui para bater o ponto
       </h4>
     </div>
-  );
-};
+  )
+}
 
-export default DoClockIn;
+export default DoClockIn
