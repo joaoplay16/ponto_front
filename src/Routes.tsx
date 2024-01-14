0e93bbf7cf937a1e8f0f57494be00596301f911b
João Pedro <joaoplay16@gmail.com>
2024-01-14 14:47:43 -0300

    Configura rotas de usuario

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,12 @@
-import { lazy, Suspense } from "react"
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from "./App"
+import { navigationRoutes as navRoutes } from "./RoutePaths"
+import { lazy, Suspense } from "react"
+
+const Dashboard = lazy(() => import("./pages/user/Dashboard"))
+const ClockInReport = lazy(() => import("./pages/user/ClockInReport"))
+const WorkingHoursReport = lazy(() => import("./pages/user/WorkingHoursReport"))
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -11,10 +14,30 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        // element: <HomePage />,
+        element: (
+          <Suspense fallback={<div>Carregando</div>}>
+            <Dashboard />
+          </Suspense>
+        )
       },
-    ],
-  },
+      {
+        path: navRoutes.user.report.wourkingHours,
+        element: (
+          <Suspense fallback={<div>Carregando</div>}>
+            <WorkingHoursReport />
+          </Suspense>
+        )
+      },
+      {
+        path: navRoutes.user.report.clockIn,
+        element: (
+          <Suspense fallback={<div>Carregando</div>}>
+            <ClockInReport />
+          </Suspense>
+        )
+      }
+    ]
+  }
 ])
 
 export function Routes() {
