deedeb3cf5f80fa064c8dabd9533c35b5c2c1c6f
João Pedro <joaoplay16@gmail.com>
2024-01-14 18:01:46 -0300

    Cria componente de mensagem de erro para campos de form

---
+++ src/components/ValidationError.tsx
@@ -0,0 +1,19 @@
+import { FieldError } from "react-hook-form"
+type Props = {
+  fieldError: FieldError | undefined
+}
+function ValidationError({ fieldError }: Props) {
+  if (!fieldError) {
+    return null
+  }
+  return (
+    <div
+      role="alert"
+      className="text-red-500 text-xs mt-1">
+      {fieldError.message}
+    </div>
+  )
+}
+
+
+export default ValidationError
--- src/components/index.ts
+++ src/components/index.ts
@@ -10,4 +10,5 @@ export { default as  CardInfo} from "./CardInfo"
 export { default as  Select} from "./Select"
 export { default as  WorkingHoursTable} from "./WorkingHoursTable"
 export { default as  Button} from "./Button"
+export { default as  ValidationError} from "./ValidationError"
 
