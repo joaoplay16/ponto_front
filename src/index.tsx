39d4be016f5ac7b2857757de1c41b6f2ceb180b3
João Pedro <joaoplay16@gmail.com>
2024-01-14 00:13:34 -0300

    Implementa context de autenticacao na aplicacao

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import reportWebVitals from "./reportWebVitals"
 import ErrorBoundary from "./Error"
 import Root from "./Root"
 import "./index.css"
+import { AuthProvider } from "./contexts/auth"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <Root />
+      <AuthProvider>
+        <Root />
+      </AuthProvider>
     </ErrorBoundary>
   </React.StrictMode>
 )
