0f5d162f98e898f393f2251ac4b459960099a199
João Pedro <joaoplay16@gmail.com>
2024-01-14 00:12:31 -0300

    Cria contexto para autenticacao do usuario

---
+++ src/contexts/auth.tsx
@@ -0,0 +1,63 @@
+import React, { ReactNode, createContext, useContext, useState } from "react"
+import { Usuario } from "../types"
+import { apiService } from "../services/apiService"
+
+interface UserInfo {
+  isUserLoggedIn: boolean
+  user: Usuario | null
+}
+
+export interface AuthContextProps {
+  login: (email: string, password: string) => void
+  logout: () => void
+  userInfo: UserInfo
+  setUserInfo: React.Dispatch<React.SetStateAction<UserInfo>>
+}
+
+const AuthContext = createContext<AuthContextProps | null>(null)
+
+function AuthProvider({ children }: { children: ReactNode }) {
+  const [userInfo, setUserInfo] = useState<UserInfo>({
+    isUserLoggedIn: false,
+    user: null
+  })
+  const login = async (email: string, password: string) => {
+    const user = await apiService.login(email, password)
+    setUserInfo({
+      isUserLoggedIn: true,
+      user: user
+    })
+  }
+
+  const logout = () => {
+    apiService.logout()
+    setUserInfo({
+      isUserLoggedIn: true,
+      user: null
+    })
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        login,
+        logout,
+        userInfo,
+        setUserInfo
+      }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth() {
+  const context = useContext(AuthContext)
+
+  if (!context) {
+    throw new Error("AuthContext deve ser usado com <AuthContext.Provider>")
+  }
+
+  return context
+}
+
+export { AuthProvider, AuthContext, useAuth }
