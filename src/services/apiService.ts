d610f6b5850e2e56dbaec074d48690629de20f6a
João Pedro <joaoplay16@gmail.com>
2024-01-13 21:57:03 -0300

    Adiciona funcao para registro de ponto no servico de api

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -13,14 +13,19 @@ export type ClockInReporParamns = {
 }
 type ApiService = {
   userClockInReport: (params: ClockInReporParamns) => Promise<PontosData>
+  doClockIn: (userId: number) => Promise<void>
 }
 
 export const apiService: ApiService = {
-  userClockInReport: async ({ userId, limit = 10, offset = 0}) => {
+  userClockInReport: async ({ userId, limit = 10, offset = 0 }) => {
     return (
       await api.get<PontosData>(
         `/usuario/${userId}/pontos?limit=${limit}&offset=${offset}`
       )
     ).data
+  },
+  // http://localhost:8088/usuario/1/registrar_ponto
+  doClockIn: async (userId: number) => {
+    return await api.get(`/usuario/${userId}/registrar_ponto`)
   }
 }
