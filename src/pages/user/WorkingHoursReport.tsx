bddf816d713d25d3730e8ea9c86ee96aaabfea32
João Pedro <joaoplay16@gmail.com>
2024-01-14 14:35:24 -0300

    Adiciona titulo e subtitulo no relatorio de horas trabalhadas

--- src/pages/user/WorkingHoursReport.tsx
+++ src/pages/user/WorkingHoursReport.tsx
@@ -1,12 +1,20 @@
-import { CardInfo, WorkingHoursTable } from "../../components"
+import { CardInfo, ScreenTitle, WorkingHoursTable } from "../../components"
 
 const WorkingHoursReport = () => {
   return (
-    <div className="flex flex-wrap gap-4 md:gap-16">
-      <div className="">
-        <WorkingHoursTable defaultPerPage={12} pagination userId={1} />
+    <div>
+      <ScreenTitle title="Relatório de horas trabalhadas" />
+      <hr className="border-t-2" />
+      <h5 className="my-1 text-sm text-gray-600 md:my-3 md:text-[20px] ">
+        Bem-vindo ao seu relatório de horas trabalhadas. Aqui estão algumas
+        informações essenciais
+      </h5>
+      <div className="flex flex-wrap gap-4 md:gap-16">
+        <div className="">
+          <WorkingHoursTable defaultPerPage={12} pagination userId={1} />
+        </div>
+        <CardInfo />
       </div>
-        <CardInfo/>
     </div>
   )
 }
