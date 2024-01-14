e1f491d7fd1241d2509ad9fdd9fd17e50879ec06
João Pedro <joaoplay16@gmail.com>
2024-01-14 09:43:34 -0300

    Cria componente item de dashboard

---
+++ src/components/DashboarItem.tsx
@@ -0,0 +1,21 @@
+type DashboarItemProps = {
+  title: string
+  info: string
+  smallInfo: string
+}
+
+const DashboarItem = ({ title, info, smallInfo }: DashboarItemProps) => {
+  return (
+    <div className="flex h-24 w-48 flex-col justify-between rounded-xl bg-gray-200 px-3 py-2 text-slate-600 shadow-md">
+      <div className="flex w-full items-baseline justify-between">
+        <span className="text-sm md:text-[22px] font-medium ">{title}</span>
+        <span className="flex-shrink-0 rounded-full bg-cyan-900/60 px-2 text-[14px] text-slate-100">
+          {smallInfo}
+        </span>
+      </div>
+      <span className="text-sm md:text-[22px]">{info}</span>
+    </div>
+  )
+}
+
+export default DashboarItem
