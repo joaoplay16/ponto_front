0cc98f111a65f69a4059e9a4ff323d31ea1940c2
João Pedro <joaoplay16@gmail.com>
2024-01-14 09:24:09 -0300

    Corrige o layout
    Layout colapsando ao adicionar itens ao conteudo principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Header, SideBarForUser } from "./components"
 import { Outlet } from "react-router-dom"
 
@@ -6,22 +6,24 @@ function App() {
   const [isMenuOpen, setMenuOpen] = useState(true)
 
   return (
-    <div className="flex h-screen ">
-      <SideBarForUser
-        className={`w-0  bg-gray-700 ${
-          isMenuOpen ? "md:w-52" : "md:w-0"
-        } flex-shrink-0 duration-200 `}
-      />
-      <div className="h-full w-full">
-        <Header
-          className="bg-gray-700 md:flex-grow"
-          isMenuOpen={isMenuOpen}
-          openMenu={() => {
-            setMenuOpen(!isMenuOpen)
-          }}
+    <div className="flex h-screen flex-col">
+      <div className="flex flex-1 ">
+        <SideBarForUser
+          className={`w-0  bg-gray-700 ${
+            isMenuOpen ? "md:w-52" : "md:w-0"
+          } sticky top-0 h-screen flex-shrink-0 duration-200`}
         />
-        <div className="flex flex-col p-4 md:p-10">
-          <Outlet />
+        <div className="h-full w-full">
+          <Header
+            className="sticky top-0 bg-gray-700 md:flex-grow"
+            isMenuOpen={isMenuOpen}
+            openMenu={() => {
+              setMenuOpen(!isMenuOpen)
+            }}
+          />
+          <div className="flex flex-1 flex-col overflow-y-auto p-4 md:p-10">
+            <Outlet />
+          </div>
         </div>
       </div>
     </div>
