e0d2df2a87afff4b1fbd4ef75de6296ac6ef8b1c
João Pedro <joaoplay16@gmail.com>
2024-01-14 14:33:25 -0300

    Adiciona titulo e subtitulo no relatorio de ponto

--- src/pages/user/ClockInReport.tsx
+++ src/pages/user/ClockInReport.tsx
@@ -1,17 +1,28 @@
-import { CardInfo } from "../../components"
+import { CardInfo, ScreenTitle } from "../../components"
 import ClockInTable from "../../components/ClockInTable"
 import { useAuth } from "../../contexts"
 
 const ClockInReport = () => {
-
   const { userInfo } = useAuth()
 
   return (
-    <div className="flex flex-wrap gap-4 md:gap-16">
-      <div className="">
-        <ClockInTable defaultPerPage={12} pagination userId={userInfo.user?.id || 0} />
+    <div>
+      <ScreenTitle title="Relatório de ponto" />
+      <hr className="border-t-2" />
+      <h5 className="text-gray-600 text-sm md:text-[20px] my-1 md:my-3 ">
+        Bem-vindo ao seu relatório de ponto. Aqui estão algumas informações
+        essenciais
+      </h5>
+      <div className="flex flex-wrap gap-4 md:gap-16">
+        <div className="">
+          <ClockInTable
+            defaultPerPage={12}
+            pagination
+            userId={userInfo.user?.id || 0}
+          />
+        </div>
+        <CardInfo />
       </div>
-        <CardInfo/>
     </div>
   )
 }
