f086ad16fd82ae1ca394e353350acb44c85aeef8
João Pedro <joaoplay16@gmail.com>
2024-01-14 14:23:01 -0300

    Obtem usuario do contexto para inicializar ClockInTable

--- src/pages/user/ClockInReport.tsx
+++ src/pages/user/ClockInReport.tsx
@@ -1,11 +1,15 @@
 import { CardInfo } from "../../components"
 import ClockInTable from "../../components/ClockInTable"
+import { useAuth } from "../../contexts"
 
 const ClockInReport = () => {
+
+  const { userInfo } = useAuth()
+
   return (
     <div className="flex flex-wrap gap-4 md:gap-16">
       <div className="">
-        <ClockInTable defaultPerPage={12} pagination userId={1} />
+        <ClockInTable defaultPerPage={12} pagination userId={userInfo.user?.id || 0} />
       </div>
         <CardInfo/>
     </div>
