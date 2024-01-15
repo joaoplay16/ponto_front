826de1b1640b53dec692aea7e3d4bbc7bac070d2
João Pedro <joaoplay16@gmail.com>
2024-01-15 09:39:27 -0300

    Configura rotas dinamicas
    Retorna rotas a depender do estado 'logado' e tipo usuario

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,20 @@
 import { Routes } from "./Routes"
+import { AdminRoutes } from "./AdminRoutes"
+import { useAuth } from "./contexts"
+import { AuthRoutes } from "./AuhtRoutes"
 
 const Root = () => {
-  return <Routes />
+  const { userInfo } = useAuth()
+
+  if (userInfo.user == null) {
+    return <AuthRoutes />
+  }
+
+  if (userInfo.user.e_admin) {
+    return <AdminRoutes />
+  } else {
+    return <Routes />
+  }
 }
 
 export default Root
