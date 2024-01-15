d52463b5d55199c6c76c48d4e50f04e27e72ddae
João Pedro <joaoplay16@gmail.com>
2024-01-15 09:36:32 -0300

    Configura layout principal
    Para exibir conforme o tipo de usuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import { useEffect, useState } from "react"
-import { Header, SideBarForUser } from "./components"
+import { useState } from "react"
+import {
+  Header,
+  HeaderForAdmin,
+  SideBarForUser,
+  SidebarForAdmin
+} from "./components"
 import { Outlet } from "react-router-dom"
+import { useAuth } from "./contexts/"
 
 function App() {
   const [isMenuOpen, setMenuOpen] = useState(true)
 
+  const { userInfo } = useAuth()
+
+  const isAdmin = userInfo.user?.e_admin == 1
+
   return (
     <div className="flex h-screen flex-col">
       <div className="flex flex-1 ">
-        <SideBarForUser
-          className={`w-0  bg-gray-700 ${
-            isMenuOpen ? "md:w-52" : "md:w-0"
-          } sticky top-0 h-screen flex-shrink-0 duration-200`}
-        />
-        <div className="h-full w-full">
-          <Header
-            className="sticky top-0 bg-gray-700 md:flex-grow z-10"
-            isMenuOpen={isMenuOpen}
-            openMenu={() => {
-              setMenuOpen(!isMenuOpen)
-            }}
+        {!isAdmin && (
+          <SideBarForUser
+            className={`w-0  bg-gray-700 ${
+              isMenuOpen ? "md:w-52" : "md:w-0"
+            } sticky top-0 h-screen flex-shrink-0 duration-200`}
           />
+        )}
+        {isAdmin && (
+          <SidebarForAdmin
+            className={`w-0  bg-gray-700 ${
+              isMenuOpen ? "md:w-52" : "md:w-0"
+            } sticky top-0 h-screen flex-shrink-0 duration-200`}
+          />
+        )}
+        <div className="h-full w-full">
+          {!isAdmin && (
+            <Header
+              className="sticky top-0 z-10 bg-gray-700 md:flex-grow"
+              isMenuOpen={isMenuOpen}
+              openMenu={() => {
+                setMenuOpen(!isMenuOpen)
+              }}
+            />
+          )}
+          {isAdmin && (
+            <HeaderForAdmin
+              className="sticky top-0 z-10 bg-gray-700 md:flex-grow"
+              isMenuOpen={isMenuOpen}
+              openMenu={() => {
+                setMenuOpen(!isMenuOpen)
+              }}
+            />
+          )}
           <div className="flex flex-1 flex-col overflow-y-auto p-4 md:p-10">
             <Outlet />
           </div>
