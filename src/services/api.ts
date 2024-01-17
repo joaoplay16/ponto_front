a2f31eda1333468b53788d64b5726376b2fdd5bd
João Pedro <joaoplay16@gmail.com>
2024-01-16 23:29:22 -0300

    Adiciona withCredentials na configuração do axios
    Inclui cookies na requisição para asegurar que a sessão não seja destruida a cada mudança de rota na aplicação

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,10 @@ import axios from "axios"
 
 const API_HOST = process.env.REACT_APP_API_HOST
 
+// withCredentials: true ára ter certeza que inclui os cookies na requisição
 const api = axios.create({
   baseURL: API_HOST,
+  withCredentials: true
 })
 
 export default api
