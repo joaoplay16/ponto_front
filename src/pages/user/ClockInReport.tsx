7cf0c6d589000a450f42aad7934b7d24b9625de7
João Pedro <joaoplay16@gmail.com>
2024-01-17 20:18:56 -0300

    Assegura que o usuário exista antes de exibir o relatório pontos

--- src/pages/user/ClockInReport.tsx
+++ src/pages/user/ClockInReport.tsx
@@ -4,22 +4,20 @@ import { useAuth } from "../../contexts"
 
 const ClockInReport = () => {
   const { userInfo } = useAuth()
-
+  const userId = userInfo.user?.id
   return (
     <div>
       <ScreenTitle title="Relatório de ponto" />
       <hr className="border-t-2" />
-      <h5 className="text-gray-600 text-sm md:text-[20px] my-1 md:my-3 ">
+      <h5 className="my-1 text-sm text-gray-600 md:my-3 md:text-[20px] ">
         Bem-vindo ao seu relatório de ponto. Aqui estão algumas informações
         essenciais
       </h5>
       <div className="flex flex-wrap gap-4 md:gap-16">
         <div className="">
-          <ClockInTable
-            defaultPerPage={12}
-            pagination
-            userId={userInfo.user?.id || 0}
-          />
+          {userId && (
+            <ClockInTable defaultPerPage={12} pagination userId={userId} loadTrigger={0}/>
+          )}
         </div>
         <CardInfo />
       </div>
