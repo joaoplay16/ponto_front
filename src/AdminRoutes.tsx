83e8555ee7079bd8adaad4bcd3631a4a666ec041
João Pedro <joaoplay16@gmail.com>
2024-01-18 10:00:49 -0300

    Remove rota de login do router de admin
    Não é necessária, pois já AuthRoutes já lida com isso

--- src/AdminRoutes.tsx
+++ src/AdminRoutes.tsx
@@ -8,7 +8,6 @@ const WorkingHoursReport = lazy(() => import("./pages/admin/WorkingHoursReport")
 const UsersReport = lazy(() => import("./pages/admin/UsersReport"))
 const UserRegister = lazy(() => import("./pages/admin/UserRegister"))
 const UserProfile = lazy(() => import("./pages/admin/UserProfile"))
-const Login = lazy(() => import("./pages/common/Login"))
 
 const router = createBrowserRouter([
   {
@@ -68,15 +67,7 @@ const router = createBrowserRouter([
         )
       },
     ]
-  },
-  {
-    path: navRoutes.login,
-    element: (
-      <Suspense fallback={<div>Carregando</div>}>
-        <Login />
-      </Suspense>
-    )
-  },
+  }
 ])
 
 export function AdminRoutes() {
