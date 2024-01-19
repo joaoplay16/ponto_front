c8be17ec2cc1394d95d33f856d8f5f6ec0a364fb
João Pedro <joaoplay16@gmail.com>
2024-01-19 09:42:35 -0300

    Ajusta layout do item de dashboard

--- src/components/DashboarItem.tsx
+++ src/components/DashboarItem.tsx
@@ -6,7 +6,7 @@ type DashboarItemProps = {
 
 const DashboarItem = ({ title, info, smallInfo }: DashboarItemProps) => {
   return (
-    <div className="flex h-24 w-48 flex-col justify-between rounded-xl bg-gray-200 px-3 py-2 text-slate-600 shadow-md">
+    <div className="flex h-24 w-48 sm:w-56 sm:h-28 flex-col justify-between rounded-xl bg-gray-200 px-3 py-2 text-slate-600 shadow-md">
       <div className="flex w-full items-baseline justify-between">
         <span className="text-sm md:text-[22px] font-medium ">{title}</span>
         <span className="flex-shrink-0 rounded-full bg-cyan-900/60 px-2 text-[14px] text-slate-100">
