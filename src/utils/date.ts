58b117cd3e0731f1cefe69c18fb9bd5b01e7e7bf
João Pedro <joaoplay16@gmail.com>
2024-01-18 22:45:43 -0300

    Cria função utilitária para formatar datas
    Do formato YYYY-MM-DD para DD/MM/YYYY

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -23,4 +23,10 @@ function getMonth(month: number): string | undefined {
   }
 }
 
-export { getMonth }
+function formatDate(date: string){
+  const [year, month, day] = date.split("-")
+
+  return `${day}/${month}/${year}`
+}
+
+export { getMonth, formatDate }
