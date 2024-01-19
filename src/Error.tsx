512d4af31e12eef47f7366c47795a3aac0914a99
João Pedro <joaoplay16@gmail.com>
2024-01-18 22:27:45 -0300

    Cria o layout para erro geral

--- src/Error.tsx
+++ src/Error.tsx
@@ -1,4 +1,5 @@
 import React, { Component, ErrorInfo } from "react"
+import { Bulb } from "./assets"
 
 interface Props {
   children: React.ReactNode
@@ -22,10 +23,17 @@ class ErrorBoundary extends Component<Props, State> {
   render() {
     if (this.state.hasError) {
       return (
-        <div>
-          <h1>Algo deu errado!</h1>
-          <p>Desculpe, ocorreu um erro inesperado.</p>
+        <div className="flex flex-col md:flex-row  gap-8 md:gap-4 h-screen w-screen items-center justify-center p-4 text-gray-700">
+        <div className="">
+          <h1 className="text-[#C76B43] text-2xl md:text-4xl">
+            OOPS! <span className="text-gray-600">Algo deu errado</span>
+          </h1>
+          <h2 className="text-lg md:text-2xl md:pt-2">
+            Desculpe, ocorreu um erro inesperado.
+          </h2>
         </div>
+          <img className="order-first md:-order-last" src={Bulb} width={250} height={250} alt="" />
+      </div>
       )
     }
 
